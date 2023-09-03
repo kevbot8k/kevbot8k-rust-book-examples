08b432e2088332eb402756dc44c53679a9a36ff0
Kenan Farmer <kenanfarmer@gmail.com>
2023-09-03 10:06:55 -0400

    updated with a loop and random number generator

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -1,18 +1,39 @@
 // docs at https://doc.rust-lang.org/std/io/
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let rand_num = rand::thread_rng().gen_range(1..101);
 
-    // mutable String builder (not str)
-    let mut guess = String::new();
+    println!("The secret number is: {rand_num}");
 
-    // need expect for Err handling
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        // mutable String builder (not str)
+        let mut guess = String::new();
+
+        // need expect for Err handling. guess is modified as side effect
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // shadowing. sequentially?
+        let guess: u32 = match guess.trim().parse() {
+            Ok(x) => x,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&rand_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
